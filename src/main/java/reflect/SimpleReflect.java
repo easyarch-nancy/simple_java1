b1eb70ee1cf91a01aaf4b80b51e434c7fe1f4670
easyarch-nancy <3179087516@qq.com>
2021-03-30 21:55:37 +0800

    Merge remote-tracking branch 'origin/master'

---
+++ src/main/java/reflect/SimpleReflect.java
@@ -0,0 +1,42 @@
+package reflect;
+
+import reflect.entity.Person;
+
+import java.lang.reflect.Field;
+
+/**
+ * @author wei.jiang
+ * @since 2018/11/22
+ */
+public class SimpleReflect {
+    public static void main(String[] args) throws
+            ClassNotFoundException,
+            IllegalAccessException,
+            InstantiationException,
+            NoSuchFieldException{
+        //一般的创建对象
+        Person p = new Person();
+
+        //反射的创建对象
+        String className = "reflect.entity.Person";
+        Class<?> c =Class.forName(className);
+        Object obj = c.newInstance();
+
+        //获取私有字段
+        Field nameField = c.getDeclaredField("name");
+        Field ageField = c.getDeclaredField("age");
+
+        //set方法赋值（暴力访问）
+        nameField.setAccessible(true);
+        ageField.setAccessible(true);
+
+        nameField.set(obj,"aaaa");
+        ageField.set(obj,11);
+
+        System.out.println("name:"+nameField.get(obj));
+        System.out.println("age:"+ageField.get(obj));
+        p=(Person) obj;
+        System.out.println("字段设置的值：name=" + p.getName() + ",age=" + p.getAge());
+
+    }
+}
---
+++ src/main/java/reflect/entity/Person.java
@@ -0,0 +1,52 @@
+package reflect.entity;
+
+/**
+ * @author wei.jiang
+ * @since 2018/11/22
+ */
+
+public class Person {
+    private String id;
+    private  String name;
+    private  int age;
+
+    public String getId(){return id;}
+
+    public void setId(String id){this.id=id;}
+
+    public String getName(){
+        return name;
+    }
+
+    public void setName(String name){this.name = name;}
+
+    public int getAge(){return age;}
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public static void fun1() {
+        System.out.println("function1");
+    }
+
+    public void fun2() {
+        System.out.println("function2");
+    }
+
+    public void fun3(String p) {
+        System.out.println(p + "---->fun3");
+    }
+
+    public void fun5(String... str) {
+
+    }
+
+    public void fun6(String[] str){
+
+    }
+
+    @SuppressWarnings("unused")
+    private void fun4() {
+        System.out.println("function4");
+    }
+}
